9c215d3a1084d9db00b28a4cc0488b012d245053
Henrique <henriquesg09@hotmail.com>
2022-08-17 23:25:02 -0300

    chore: add plausible script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
+        <script
+          defer
+          data-domain='antitermo.hnqg.io'
+          src='https://plausible.io/js/plausible.js'
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,15 +10,9 @@ class MyDocument extends Document {
     return (
       <Html lang='pt-BR'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
           <link
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap'
             rel='stylesheet'
-          />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0, viewport-fit=cover'
           />
         </Head>
         <body>
